ece31cb55d9e602549dd0839f6c7c759a8fa8a54
Ivan Baksheev <zpix-dev@list.ru>
2025-02-14 00:25:12 +0700

    Untranslate audio (#32)
    * WIP
    
    * v1.5.15

--- app/src/options.js
+++ app/src/options.js
@@ -1,46 +1,82 @@
 function saveOptions() {
-    if (document.getElementById('reload-checkbox').checked) {
-        chrome.tabs.reload();
+  if (document.getElementById("reload-checkbox").checked) {
+    chrome.tabs.reload();
+  }
+  chrome.storage.sync.get(
+    {
+      disabled: false,
+      autoreloadOption: true,
+      untranslateAudio: false,
+    },
+    function (items) {
+      let disabled = !items.disabled;
+      chrome.storage.sync.set(
+        {
+          disabled: disabled,
+        },
+        function () {
+          document.getElementById("disable-button").innerText = disabled
+            ? "Enable"
+            : "Disable";
+          document.getElementById("status").innerText = disabled
+            ? "Disabled"
+            : "Enabled";
+          document.getElementById("status").className =
+            "status " + (disabled ? "disabled" : "enabled");
+          document.getElementById("disable-button").className = disabled
+            ? "disabled"
+            : "enabled";
+        }
+      );
     }
-    chrome.storage.sync.get({
-        disabled: false,
-        autoreloadOption: true
-    }, function (items) {
-        let disabled = !items.disabled;
-        chrome.storage.sync.set({
-            disabled: disabled
-        }, function () {
-            document.getElementById('disable-button').innerText = disabled ? 'Enable' : 'Disable';
-            document.getElementById('status').innerText = disabled ? 'Disabled' : 'Enabled';
-            document.getElementById('status').className = 'status ' + (disabled ? 'disabled' : 'enabled');
-            document.getElementById('disable-button').className = disabled ? 'disabled' : 'enabled';
-        });
-    });
+  );
 }
 
 function loadOptions() {
-    chrome.storage.sync.get({
-        disabled: false,
-        autoreloadOption: true
-    }, function (items) {
-        document.getElementById('disable-button').innerText = items.disabled ? 'Enable' : 'Disable';
-        document.getElementById('status').innerText = items.disabled ? 'Disabled' : 'Enabled';
-        document.getElementById('status').className = 'status ' + (items.disabled ? 'disabled' : 'enabled');
-        document.getElementById('disable-button').className = items.disabled ? 'disabled' : 'enabled';
-        document.getElementById('reload-checkbox').checked = items.autoreloadOption;
-    });
+  chrome.storage.sync.get(
+    {
+      disabled: false,
+      autoreloadOption: true,
+      untranslateAudio: false,
+    },
+    function (items) {
+      document.getElementById("disable-button").innerText = items.disabled
+        ? "Enable"
+        : "Disable";
+      document.getElementById("status").innerText = items.disabled
+        ? "Disabled"
+        : "Enabled";
+      document.getElementById("status").className =
+        "status " + (items.disabled ? "disabled" : "enabled");
+      document.getElementById("disable-button").className = items.disabled
+        ? "disabled"
+        : "enabled";
+      document.getElementById("reload-checkbox").checked =
+        items.autoreloadOption;
+      document.getElementById("audio-checkbox").checked =
+        items.untranslateAudio;
+    }
+  );
 }
 
 function checkboxUpdate() {
-    chrome.storage.sync.set({
-        autoreloadOption: document.getElementById('reload-checkbox').checked
-    });
+  chrome.storage.sync.set({
+    autoreloadOption: document.getElementById("reload-checkbox").checked,
+    untranslateAudio: document.getElementById("audio-checkbox").checked,
+  });
 }
 
 function addListeners() {
-    document.getElementById('disable-button').addEventListener('click', saveOptions);
-    document.getElementById('reload-checkbox').addEventListener('click', checkboxUpdate);
+  document
+    .getElementById("disable-button")
+    .addEventListener("click", saveOptions);
+  document
+    .getElementById("reload-checkbox")
+    .addEventListener("click", checkboxUpdate);
+  document
+    .getElementById("audio-checkbox")
+    .addEventListener("click", checkboxUpdate);
 }
 
-document.addEventListener('DOMContentLoaded', loadOptions);
-document.addEventListener('DOMContentLoaded', addListeners);
+document.addEventListener("DOMContentLoaded", loadOptions);
+document.addEventListener("DOMContentLoaded", addListeners);
--- app/src/start.js
+++ app/src/start.js
@@ -1,12 +1,23 @@
 // To run in page environment
 
-chrome.storage.sync.get({
-    disabled: false
-}, function (items) {
+chrome.storage.sync.get(
+  {
+    disabled: false,
+    untranslateAudio: false,
+  },
+  function (items) {
     if (!items.disabled) {
-        var s = document.createElement('script');
-        s.defer = true;
-        s.src = chrome.runtime.getURL('src/background.js');
-        document.body.appendChild(s);
+      var backgroundText = document.createElement("script");
+      backgroundText.defer = true;
+      backgroundText.src = chrome.runtime.getURL("src/background.js");
+      document.body.appendChild(backgroundText);
+
+      if (items.untranslateAudio) {
+        var backgroundAudio = document.createElement("script");
+        backgroundAudio.defer = true;
+        backgroundAudio.src = chrome.runtime.getURL("src/background_audio.js");
+        document.body.appendChild(backgroundAudio);
+      }
     }
-});
+  }
+);
