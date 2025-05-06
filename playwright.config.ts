9d321f72d5b76ad01e24e143cfe4aef11b864803
namakeingo <pzzdave95@gmail.com>
2025-05-06 11:05:43 +0100

    update playwright settings and rename background file for descriptions

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,10 +38,10 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium-extension",
+      name: "firefox-extension",
       testMatch: /.*extension\.spec\.ts/,
       use: {
-        ...devices["Desktop Chrome"],
+        ...devices["Desktop Firefox"],
         contextOptions: {
           // Load the extension from the app directory
           permissions: ["clipboard-read", "clipboard-write"],
