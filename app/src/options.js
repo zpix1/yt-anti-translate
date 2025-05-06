103fcad7f3bed097558298e0da29308d04867a68
namakeingo <pzzdave95@gmail.com>
2025-05-06 12:57:06 +0100

    firefox manifest v3
    this was very easy in manifest v3

--- app/src/options.js
+++ app/src/options.js
@@ -27,36 +27,8 @@ function saveOptions() {
           document.getElementById("disable-button").className = disabled
             ? "disabled"
             : "enabled";
-		  tellThePage();
         }
       );
-    }
-  );
-}
-
-function tellThePage(){
-  let message;
-  if (document.getElementById('status').innerText == "Enabled"){ 
-    //The value is set correctly
-    message = "Enable";
-  }
-  else if (document.getElementById('status').innerText == "Disabled")
-  {
-    message = "Disable";
-  }
-  browser.tabs.query(
-    { url:"*://*.youtube.com/*" }, 
-    function (tabs) {
-	  for (var x = 0; x < tabs.length; x++) {
-        //sending a message to content script https://stackoverflow.com/questions/14245334/sendmessage-from-extension-background-or-popup-to-content-script-doesnt-work
-        browser.tabs.sendMessage(tabs[x].id, message)
-          .then(response => { 
-            console.log("Message from the content script:",response);
-		  })
-          .catch(e => {
-            console.log(e);
-          });
-      }  
     }
   );
 }
