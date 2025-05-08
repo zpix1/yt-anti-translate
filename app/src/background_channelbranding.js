7a1143892adce2ee27736d77bc224d9ebfb6d673
namakeingo <namakeingo@gmail.com>
2025-05-08 13:24:55 +0100

    Update background_channelbranding.js
    Additional daft for Player Channel Name

--- app/src/background_channelbranding.js
+++ app/src/background_channelbranding.js
@@ -2,6 +2,7 @@
 const LOG_PREFIX = "[YoutubeAntiTranslate]";
 const CHANNEL_HEADER_SELECTOR = "#page-header-container #page-header .page-header-view-model-wiz__page-header-headline-info";
 const CHANNEL_ABOUT_SELECTOR = "#about-container";
+const PLAYER_CHANNEL_NAME_SELECTOR = 'ytd-video-owner-renderer ytd-channel-name .ytd-channel-name a.yt-simple-endpoint'
 const ATTRIBUTED_STRING_SELECTOR = "yt-attributed-string";
 const MUTATION_UPDATE_FREQUENCY = 2;
 
@@ -14,6 +15,11 @@ function mutationBrandingHeaderCounter() { }
  * Processes the about and restores it to its original form
  */
 function restoreOriginalAbout() { }
+
+/**
+ * Processes the player channel name and restores it to its original form
+ */
+function restoreOriginalPlayerChannelName() { }
 
 /**
  * Handles channel branding header updates when mutations are detected
@@ -45,6 +51,23 @@ async function handleAboutMutation() {
   mutationAboutCounter++;
 }
 
+
+
+/**
+ * Handles channel about updates when mutations are detected
+ */
+let mutationPlayerChannelNameCounter = 0;
+
+async function handlePlayerChannelNameMutation() {
+  if (mutationPlayerChannelNameCounter % MUTATION_UPDATE_FREQUENCY === 0) {
+    const PlayerChannelNameElement = document.querySelector(PLAYER_CHANNEL_NAME_SELECTOR);
+    if (PlayerChannelNameElement) {
+      restoreOriginalPlayerChannelName();
+    }
+  }
+  mutationPlayerChannelNameCounter++;
+}
+
 // Initialize the mutation observer for branding header and about
 const targetNode = document.body;
 const observerConfig = { childList: true, subtree: true };
@@ -52,3 +75,5 @@ const brandingHeaderObserver = new MutationObserver(handleBrandingHeaderMutation
 brandingHeaderObserver.observe(targetNode, observerConfig);
 const aboutObserver = new MutationObserver(handleAboutMutation);
 aboutObserver.observe(targetNode, observerConfig);
+const playerChannelNameObserver = new MutationObserver(handlePlayerChannelNameMutation);
+playerChannelNameObserver.observe(targetNode, observerConfig);
