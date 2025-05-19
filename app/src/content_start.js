27c37a2e642038eded718c2eb54c8e372f865da8
namakeingo <namakeingo@gmail.com>
2025-05-19 12:58:20 +0100

    Update content_start.js

--- app/src/content_start.js
+++ app/src/content_start.js
@@ -7,10 +7,10 @@ chrome.storage.sync.get(
   },
   async function (items) {
     if (!items.disabled) {
-      const constScript = document.createElement("script");
-      constScript.type = "module";
-      constScript.src = chrome.runtime.getURL("src/content_constants.js");
-      document.body.appendChild(constScript);
+      const globalPropertiesScript = document.createElement("script");
+      globalPropertiesScript.type = "module";
+      globalPropertiesScript.src = chrome.runtime.getURL("src/global.js");
+      document.body.appendChild(globalPropertiesScript);
 
       const backgroundScript = document.createElement("script");
       backgroundScript.type = "module";
