b0371b7b46e56f40f2b440f1b2afc1c92e0a31dc
Ivan Baksheev <zpix-dev@list.ru>
2025-06-02 18:12:29 +0700

    123

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig<TestOptions>({
   timeout: 960_000,
   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry 3 times on CI, or once locally */
